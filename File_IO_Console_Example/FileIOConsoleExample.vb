24c84f25ecc84caac407556fdab67666eb527b2e
Zac <zac.minidude140@Gmail.com>
2023-09-28 08:30:46 -0600

    Simple write to file test.txt (old way)

--- File_IO_Console_Example/FileIOConsoleExample.vb
+++ File_IO_Console_Example/FileIOConsoleExample.vb
@@ -2,7 +2,7 @@
 'RCET 2265
 'Fall 2023
 'File IO Console in Class Example
-'https://github.com/Minidude140
+'https://github.com/Minidude140/File_IO_Console_Example.git
 
 
 Option Explicit On
@@ -11,7 +11,17 @@ Option Strict On
 Module FileIOConsoleExample
 
     Sub Main()
+        WriteToFile()
+        Console.Read()
 
     End Sub
 
+    Sub WriteToFile()
+        'open a file called test.txt then write "Follow the White rabbit..."
+        'input is into program, output is to file
+        FileOpen(1, "test.txt", OpenMode.Output)
+        PrintLine(1, "Follow the white rabbit...")
+        FileClose(1)
+
+    End Sub
 End Module
