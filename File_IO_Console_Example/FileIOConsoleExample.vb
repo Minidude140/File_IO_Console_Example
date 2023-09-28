a44f5d7ea7074b247ef25f8f36bbcbdeba86c93c
Zac <zac.minidude140@Gmail.com>
2023-09-28 08:37:03 -0600

    added append sub

--- File_IO_Console_Example/FileIOConsoleExample.vb
+++ File_IO_Console_Example/FileIOConsoleExample.vb
@@ -11,7 +11,12 @@ Option Strict On
 Module FileIOConsoleExample
 
     Sub Main()
+        'Write file once
         WriteToFile()
+        For i = 0 To 10
+            'append 10 times
+            AppendToFile()
+        Next
         Console.Read()
 
     End Sub
@@ -20,8 +25,13 @@ Module FileIOConsoleExample
         'open a file called test.txt then write "Follow the White rabbit..."
         'input is into program, output is to file
         FileOpen(1, "test.txt", OpenMode.Output)
-        PrintLine(1, "Follow the white rabbit...")
+        PrintLine(1, "Hello, Neo")
         FileClose(1)
-
+    End Sub
+    Sub AppendToFile()
+        'open a file called test.txt and append to the end of file
+        FileOpen(1, "test.txt", OpenMode.Append)
+        PrintLine(1, "Follow the white rabbit....")
+        FileClose(1)
     End Sub
 End Module
